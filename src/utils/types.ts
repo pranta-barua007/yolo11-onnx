b1b54c47aebc4a4349d658cb0772bc85c3cda302
pranta-barua007 <prantabarua247@gmail.com>
2025-06-26 01:46:31 +0600

    feat: started refactoring, added new CustomModel type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,11 @@
 export interface Box {
-    bbox: number[];
-    class_idx: number;
-    score: number;
-    mask_weights: Float32Array;
+  bbox: number[];
+  class_idx: number;
+  score: number;
+  mask_weights: Float32Array;
+}
+
+export interface CustomModel {
+  name: string;
+  url: string;
 }
