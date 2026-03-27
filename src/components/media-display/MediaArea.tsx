94eb6fb8b6c02416511fe694515eed46ee5e8802
pranta-barua007 <prantabarua247@gmail.com>
2026-03-27 16:14:46 +0600

    fix: unhide close button on media area by removing overflow-hidden bounding box clip

--- src/components/media-display/MediaArea.tsx
+++ src/components/media-display/MediaArea.tsx
@@ -35,7 +35,7 @@ export default function MediaArea() {
 
       {/* Media Wrapper for correct overlay alignment */}
       {hasMedia && (
-        <div className="absolute inset-4 md:inset-6 flex justify-center items-center pointer-events-none overflow-hidden">
+        <div className="absolute inset-4 md:inset-6 flex justify-center items-center pointer-events-none">
           <div className="relative inline-block pointer-events-auto leading-none">
             {/* Video for camera feed */}
             <video
